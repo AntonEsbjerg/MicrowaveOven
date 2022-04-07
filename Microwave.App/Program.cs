3470c844689926db26f7fcfd31155a48655aa536
Anton <202008162@post.au.dk>
2022-04-07 12:55:18 +0200

    Commit to test if Jenkins(CI) is properly set up.

--- Microwave.App/Program.cs
+++ Microwave.App/Program.cs
@@ -34,7 +34,7 @@ namespace Microwave.App
             // Simulate a simple sequence
 
             powerButton.Press();
-
+            // Test Jenkins Webhook
             timeButton.Press();
 
             startCancelButton.Press();
