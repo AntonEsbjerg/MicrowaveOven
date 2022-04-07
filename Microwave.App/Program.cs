8dd05846d50975eabc1f07811b20d9b89daa2720
Anton <202008162@post.au.dk>
2022-04-07 13:04:52 +0200

    Third test to check if Jenkins(CI) is setup properly

--- Microwave.App/Program.cs
+++ Microwave.App/Program.cs
@@ -34,7 +34,7 @@ namespace Microwave.App
             // Simulate a simple sequence
 
             powerButton.Press();
-            // Test2 Jenkins Webhook 
+            // Test 3 Jenkins Webhook 
             timeButton.Press();
 
             startCancelButton.Press();
